d7d234a9e439b3944707609761ea1e329a2a19ad
RuteRyan <felichan98@gmail.com>
2022-04-14 15:08:03 +0200

    pulizia

---
+++ Modelli/README.txt
@@ -0,0 +1,6 @@
+il modello unet-256-cartoon(150).pth è scaricabile al link https://drive.google.com/file/d/1UdTWPK_phTg-Ge3WsDjGkaLdRVHbjFrb/view?usp=sharing
+poichè github non lo carica dato che è di dimensioni troppo grandi.
+
+
+Il modello baseline_G_A.pth è il modello pretrained disponibile al link https://inikolaeva.medium.com/make-pixel-art-in-seconds-with-machine-learning-e1b1974ba572
+che ho utilizzato come base di training per il train 1 con resnet.
--- video_processing.py
+++ video_processing.py
@@ -2,12 +2,39 @@ import numpy as np
 import cv2 as cv
 
 from PaletteGen import palette_extractor
-from GridGenerator import pixxelate
+from Downsampling import pixxelate
 
-cap = cv.VideoCapture('video.mp4')
+
+######### Registrazione da webcam #############
+
+"""
+cap = cv.VideoCapture(0)
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    out.write(frame)
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# Release everything if job is finished
+cap.release()
+out.release()
+cv.destroyAllWindows()
+"""
+
+####################################################
+
+
+################### Pixxelazione video #############
+
+cap = cv.VideoCapture('video_in.mp4')
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('output.avi', fourcc, 10.0, (int(cap.get(3)),int(cap.get(4))))
+out = cv.VideoWriter('video_out.avi', fourcc, 20.0, (int(cap.get(3)),int(cap.get(4))))
 while cap.isOpened():
     ret, frame = cap.read()
     
